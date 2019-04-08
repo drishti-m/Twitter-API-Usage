b164e4a0ddb86f550348ee34daef7ed10899650c
Drish10 <44743273+ohwhatafool@users.noreply.github.com>
2019-04-08 20:20:07 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
-consumer_key = 'Jgr3kD8UR0SxlyjR2PI0Yz58S'
-consumer_secret = 'RhudUmZPwmi0eeo90EeYF3vKUetc5wyH9ll35y3YtswReBgUR7'
-access_token = '227955563-yl9jip3kiNF9X8ibIBzr5eiTqg07QV1gJRzeNpJT'
-access_token_secret = 'Vnt7Rt0rPHWwWn01ZMeFub7sRoNx1TwB63nrKrxiI284Y'
+consumer_key = '**'
+consumer_secret = '**'
+access_token = '**'
+access_token_secret = '**'
+#'**" means secret keys, not publicly visible
